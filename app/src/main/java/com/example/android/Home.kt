0ee52c653d133e7287483bfe206458ed09f01bfd
johnnys <KmHWy1QP6Dswyp>
2022-03-11 17:07:17 -0300

    Insert logo on toolbar

--- app/src/main/java/com/example/android/Home.kt
+++ app/src/main/java/com/example/android/Home.kt
@@ -1,59 +1,45 @@
 package com.example.android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Home : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Add the following lines to create RecyclerView
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Home().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        val recyclerView = itemView.findViewById<RecyclerView>(R.id.recyclerView)
+        val btnDetails = itemView.findViewById<Button>(R.id.btnDetails)
+
+        recyclerView.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = RecyclerAdapter()
+        }
+
+        btnDetails.setOnClickListener {
+            Navigation.findNavController(itemView).navigate(R.id.details)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/android/RecyclerAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.android
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private val kode = arrayOf("d116df5",
+        "36ffc75", "f5cfe78", "5b87628",
+        "db8d14e", "9913dc4", "e120f96",
+        "466251b")
+
+    private val kategori = arrayOf("Kekayaan", "Teknologi",
+        "Keluarga", "Bisnis",
+        "Keluarga", "Hutang",
+        "Teknologi", "Pidana")
+
+    private val isi = arrayOf("pertanyaan 9",
+        "pertanyaan 11", "pertanyaan 17", "test forum",
+        "pertanyaan 12", "pertanyaan 18", "pertanyaan 20",
+        "pertanyaan 21")
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var itemKode: TextView
+        var itemKategori: TextView
+        var itemIsi: TextView
+
+        init {
+            itemKode = itemView.findViewById(R.id.kodePertanyaan)
+            itemKategori = itemView.findViewById(R.id.kategori)
+            itemIsi = itemView.findViewById(R.id.isiPertanyaan)
+
+            itemView.setOnClickListener {
+                var position: Int = getAdapterPosition()
+                val context = itemView.context
+                val intent = Intent(context, Home::class.java).apply {
+                    putExtra("NUMBER", position)
+                    putExtra("CODE", itemKode.text)
+                    putExtra("CATEGORY", itemKategori.text)
+                    putExtra("CONTENT", itemIsi.text)
+                }
+                context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_item, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemKode.text = kode[i]
+        viewHolder.itemKategori.text = kategori[i]
+        viewHolder.itemIsi.text = isi[i]
+
+    }
+
+    override fun getItemCount(): Int {
+        return kode.size
+    }
+}
